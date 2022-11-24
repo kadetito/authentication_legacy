818a51e5b422ffe5866741dcdaab124a10bfc3da
kadetito <kadetito@gmail.com>
2022-11-25 00:56:43 +0100

    next comnfig

--- next.config.js
+++ next.config.js
@@ -9,10 +9,10 @@ module.exports = {
   i18n: {
     // These are all the locales you want to support in
     // your application
-    locales: ["default", "en", "es"],
+    locales: ["en", "es"],
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
-    defaultLocale: "default",
+    defaultLocale: "es",
     // This is a list of locale domains and the default locale they
     // should handle (these are only required when setting up domain routing)
     // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
